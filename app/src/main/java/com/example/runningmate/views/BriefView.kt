b2d2151dca14fab5b50fbfa3623cb15169c69749
JungYubin853 <jungyubin01@student.ciputra.ac.id>
2024-12-20 15:46:54 +0700

    Added BriefView.kt

---
+++ app/src/main/java/com/example/runningmate/views/BriefView.kt
@@ -0,0 +1,11 @@
+package com.example.runningmate.views
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+
+@Composable
+fun BriefView(){
+    Column {
+
+    }
+}
