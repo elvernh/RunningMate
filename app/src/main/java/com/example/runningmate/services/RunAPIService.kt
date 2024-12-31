2a7d174031ed46bfa762c495317a9a710c1cab20
elvernh <87465378+elvernh@users.noreply.github.com>
2025-01-01 01:33:53 +0800

    tried to add the map

--- app/src/main/java/com/example/runningmate/services/RunAPIService.kt
+++ app/src/main/java/com/example/runningmate/services/RunAPIService.kt
@@ -3,9 +3,13 @@ package com.example.runningmate.services
 import com.example.runningmate.models.RunResponse
 import retrofit2.Call
 import retrofit2.http.Body
+import retrofit2.http.Header
 import retrofit2.http.POST
 
 interface RunAPIService {
     @POST("api/addRun")
-    fun addRun(@Body runMap: HashMap<String, Any>): Call<RunResponse>
+    fun addRun(
+        @Header("X-API-TOKEN") token: String,
+        @Body runMap: HashMap<String, Any>
+    ) : Call<RunResponse>
 }
